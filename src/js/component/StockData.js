03e44220d476a979b5819bf40e2cf2bd366d39c9
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-08 03:20:21 +0000

    StockData, completed the fetch to include console.logs, errore handling, and passing stock symbol as a prop so users can search for whatever stock they want to

--- src/js/component/StockData.js
+++ src/js/component/StockData.js
@@ -9,32 +9,45 @@ import {
   Legend,
 } from "recharts";
 
-const StockData = () => {
+const StockData = ({ stockSymbol }) => {
   const [data, setData] = useState(null);
-  const apiKey = "YOUR_ALPHA_VANTAGE_API_KEY"; // Replace with your actual API key
-  const stockSymbol = "IBM"; // Replace with the actual symbol input by the user
+  const apiKey = "YOUR_ALPHAVANTAGE_KEY"; // Replace with your actual API key
 
   useEffect(() => {
     fetch(
       `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSymbol}&apikey=${apiKey}`
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
+        if (data["Error Message"]) {
+          throw new Error(`API error! Message: ${data["Error Message"]}`);
+        }
         const transformedData = Object.entries(data["Time Series (Daily)"]).map(
           ([date, value]) => ({
             date,
             open: Number(value["1. open"]),
           })
         );
+        console.log(`Fetched and transformed data for symbol: ${stockSymbol}`);
         setData(transformedData);
       })
-      .catch((err) => console.error(err));
-  }, []); // Fetch data only once after the component is first rendered.
+      .catch((err) => {
+        console.error(
+          `Failed to fetch data for symbol: ${stockSymbol}. Error: ${err.message}`
+        );
+        setData(null);
+      });
+  }, [stockSymbol]); // Fetch data whenever the component is first rendered or the stock symbol changes.
 
   return (
     <div>
       <h2>Stock Data for {stockSymbol}</h2>
-      {data && (
+      {data ? (
         <LineChart
           width={500}
           height={300}
@@ -58,6 +71,11 @@ const StockData = () => {
             activeDot={{ r: 8 }}
           />
         </LineChart>
+      ) : (
+        <p>
+          Unable to fetch data for {stockSymbol}. Please ensure the symbol is
+          correct and try again.
+        </p>
       )}
     </div>
   );
