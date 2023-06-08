c88c1668f55cb31eaa3d76b8ed26366988f73a1d
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-08 00:18:43 +0000

    added a differet background color to the main section

--- src/js/views/LandingPage.js
+++ src/js/views/LandingPage.js
@@ -1,36 +1,43 @@
 import React from "react";
 import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
+import "../../styles/landingpage.css";
 
 const LandingPage = () => {
   return (
-    <Container className="my-5">
+    <Container fluid className="landing-page-bg py-5">
       <Row className="text-center mb-4">
         <Col>
-          <h1>Historical Stock Data Viewer</h1>
-          <p className="lead">Enter a stock symbol to get historical data</p>
+          <h1 className="text-light">Historical Stock Data Viewer</h1>
+          <p className="lead text-light">
+            Enter a stock symbol to get historical data
+          </p>
         </Col>
       </Row>
 
       <Row className="justify-content-center">
         <Col xs={12} md={6}>
-          <Form>
-            <Form.Group controlId="formStockSymbol">
-              <Form.Label>Stock Symbol</Form.Label>
-              <Form.Control type="text" placeholder="Enter stock symbol" />
-              <Form.Text className="text-muted">
-                For example: AAPL, GOOG, MSFT, etc.
-              </Form.Text>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+          <Card className="mb-4">
+            <Card.Body>
+              <Form>
+                <Form.Group controlId="formStockSymbol">
+                  <Form.Label>Stock Symbol</Form.Label>
+                  <Form.Control type="text" placeholder="Enter stock symbol" />
+                  <Form.Text className="text-muted">
+                    For example: AAPL, GOOG, MSFT, etc.
+                  </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
 
-      <Row className="mt-5">
-        <Col>
-          <Card>
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <Card bg="light">
             <Card.Body>
               <Card.Title>How to Use</Card.Title>
               <Card.Text>
