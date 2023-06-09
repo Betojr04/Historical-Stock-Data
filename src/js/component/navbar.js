617a8385b18ee24fc7dc8674f8704ca1bde21cee
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-09 03:56:59 +0000

    totally messed up everything, need to re adjust everything

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
       <Link to="/">
-        <span className="navbar-brand mb-0 h1">Search That Stock</span>
+        <span className="navbar-brand mb-0 h1 mr-4">Search That Stock</span>
       </Link>
       <div className="collapse navbar-collapse">
-        <ul className="navbar-nav ml-auto">
+        <ul className="navbar-nav">
           <li className="nav-item">
-            <Link to="/howtouse" className="nav-link">
+            <Link to="/howtouse" className="nav-link rightAlign">
               <button className="btn btn-primary">Help</button>
             </Link>
           </li>
--- src/js/views/LandingPage.js
+++ src/js/views/LandingPage.js
@@ -1,63 +1,30 @@
-import React from "react";
-import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import StockData from "../component/StockData";
 import "../../styles/landingpage.css";
-import money from "../../img/money.jpeg";
 
 const LandingPage = () => {
+  const [stockSymbol, setStockSymbol] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setStockSymbol(event.target.elements.formStockSymbol.value);
+  };
+
   return (
-    <div>
-      {/* Hero Section */}
-      <div
-        style={{
-          backgroundImage: `url(${money})`,
-          height: "50vh",
-          backgroundSize: "cover",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-          marginBottom: "2rem", // added some margin for separation
-        }}
-      >
-        <h1>Search Historical Stock Data</h1>
-        <h3>Make Your Future Rich Decisions</h3>
+    <div className="main-content">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Stock Symbol
+            <input type="text" placeholder="Enter stock symbol" />
+          </label>
+          <p>For example: AAPL, GOOG, MSFT, etc.</p>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-
-      {/* Main Content */}
-      <Container className="py-5">
-        <Row className="justify-content-center text-center mb-4">
-          <Col>
-            <h1 className="text-light">Historical Stock Data Viewer</h1>
-            <p className="lead text-light">
-              Enter a stock symbol to get historical data
-            </p>
-          </Col>
-        </Row>
-
-        <Row className="justify-content-center">
-          <Col xs={12} md={6}>
-            <Card className="mb-4">
-              <Card.Body>
-                <Form>
-                  <Form.Group controlId="formStockSymbol">
-                    <Form.Label>Stock Symbol</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter stock symbol"
-                    />
-                    <Form.Text className="text-muted">
-                      For example: AAPL, GOOG, MSFT, etc.
-                    </Form.Text>
-                  </Form.Group>
-                  <Button variant="primary" type="submit">
-                    Submit
-                  </Button>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div className="stock-data-container">
+        {stockSymbol && <StockData stockSymbol={stockSymbol} />}
+      </div>
     </div>
   );
 };
