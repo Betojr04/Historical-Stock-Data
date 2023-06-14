5ebd692f30e6d570e27f5bc42fdfd3a3b4ff2d15
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-14 23:17:35 +0000

    made some content changed to the main page

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
       <Link to="/">
-        <span className="navbar-brand mb-0 h1 mr-4">Search That Stock</span>
+        <span className="navbar-brand mb-0 h1 mr-4">Search That Stock and make some monayyyy</span>
       </Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav">
